11f7c5e7e9d76a497a466aa8c7da2012210ebf96
Stephan Hagemann <stephan.hagemann@gmail.com>
2014-02-24 19:41:29 -0700

    Merge migrations so addition and removal of shopProductSink is "migration atomic"

--- db/migrate/20140117001859_create_shop_product_sink_integration_objects.rb
+++
@@ -1,8 +0,0 @@
-class CreateShopProductSinkIntegrationObjects < ActiveRecord::Migration
-  def change
-    create_table :shop_product_sink_integration_objects do |t|
-      t.string :title
-      t.decimal :price, precision: 10, scale: 2
-    end
-  end
-end
